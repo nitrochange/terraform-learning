36b14d66f9b499bfee5dacabf9c9a81509777825
Aleksandr Romanov <sasha.sanya.romanov@mail.ru>
2024-07-22 22:54:02 +0300

    added security group

--- Section 4:  Understand Terraform basics/main.tf
+++ Section 4:  Understand Terraform basics/main.tf
@@ -155,3 +155,22 @@ resource "aws_s3_bucket_acl" "my_new_bucket_acl" {
   bucket = aws_s3_bucket.my-new-S3-bucket.id
   acl = "private"
 }
+
+resource "aws_security_group" "my-new-security-group" {
+  name        = "web_server_inbound"
+  description = "Allow inbound traffic on tcp/443"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "Allow 443 from the Internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "web_server_inbound"
+    Purpose = "Intro to Resource Blocks Lab"
+  }
+}

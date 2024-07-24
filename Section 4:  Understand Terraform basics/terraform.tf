cac7854db34be3e33907ea95396fe1d5d5606c9e
Aleksandr Romanov <sasha.sanya.romanov@mail.ru>
2024-07-24 22:07:41 +0300

    terraform basics leftovers

--- Section 4:  Understand Terraform basics/terraform.tf
+++ Section 4:  Understand Terraform basics/terraform.tf
@@ -1,3 +1,19 @@
 terraform {
-  required_version = ">=1.0.0"
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    http = {
+      source = "hashicorp/http"
+      version = "2.1.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.2"
+    }
+    local = {
+      source  = "hashicorp/local"
+    }
+  }
 }
